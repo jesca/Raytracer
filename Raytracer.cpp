4a6e85f3204b70c227ed532912df1bde44398860
jesca <jesca116@gmail.com>
2014-03-16 01:00:54 -0700

    updated diffuse and spec to use lights

--- Raytracer.cpp
+++ Raytracer.cpp
@@ -1,4 +1,5 @@
 #include "RayTracer.h"
+#include "Light.h"
 #include <vector>
 #define cout std::cout
 #define Transform Eigen::Transform<float, 4, Eigen::Projective>
@@ -15,30 +16,33 @@ void RayTracer::ambient(Color ka, Color* color){
 
 }
 
-void RayTracer::diffuse(Color kd,Color* color, Color lcolor, Vector3f n, Vector3f l){
+void RayTracer::diffuse(Color kd,Color* color, Vector3f n, DirectionalLight dlight){
     Vector3f nhat = n;
-    Vector3f lhat = l;
+    Vector3f lhat = dlight.getDir();
+    Color lcolor=dlight.getColor();
     nhat.normalize();
     lhat.normalize();
     float ndotl = fmax(nhat.dot(lhat),0.0f);
-    float red = fmax(kd.getR()*(lcolor.getR()*ndotl),0.0f);
-    float green = fmax(kd.getG()*(lcolor.getG()*ndotl),0.0f);
-    float blue = fmax(kd.getB()*(lcolor.getB()*ndotl),0.0f);
-     // color->print();
+    float red = kd.getR()*(lcolor.getR()*ndotl);
+    float green =kd.getG()*(lcolor.getG()*ndotl);
+    float blue = kd.getB()*(lcolor.getB()*ndotl);
     color->add(Color(red,green,blue));
-    // color->setR(red);
-    // color->setG(green);
-    // color->setB(blue);
+
    
 }
 
-void RayTracer::specular(Color ks,Color* color, Color lcolor, Vector3f r, Vector3f v, float p){
+void RayTracer::specular(Color ks,Color* color, Vector3f view, Vector3f normal,float p, PointLight pl){
+    Vector3f pos = pl.getPos();
+    pos.normalize();
+    float Ldotn = pos.dot(normal);
+    Vector3f r = -pos + 2*Ldotn*normal;
     Vector3f rhat = r;
-    Vector3f vhat = v;
+    Vector3f vhat = view;
     rhat.normalize();
     vhat.normalize();
     float rdotv = pow(fmax(rhat.dot(vhat),0.0f),p);
-    float red = ks.getR()*lcolor.getR()*rdotv;
+    Color lcolor=pl.getColor();
+    float red = ks.getR()*(lcolor.getR()*rdotv);
     float green = ks.getG()*(lcolor.getG()*rdotv);
     float blue = ks.getB()*(lcolor.getB()*rdotv);
      // color->print();
@@ -77,34 +81,36 @@ void RayTracer::trace(Ray& ray, int depth, Color* color){
     vector<GeometricPrimitive*> geoprimlist(2);
     geoprimlist.at(0)=&p; geoprimlist.at(1)=&p2;
     Intersection in;
-    //    if(in->getPrimitive()->intersect())
-    //        cout << "light blocked \n";
-    //        *color = Color(0,0,0);
-    //        return;
-    for(int i=0; i<2; i++){
-    if(!geoprimlist.at(i)->intersect(ray, &thit, &in)){
+    
+    //create directional light
+    Vector3f d1_dir = Vector3f(100,100,-100);
+    Color d1_col=Color(.6,.3,.6);
+    DirectionalLight dlight= DirectionalLight(d1_dir,d1_col);
+
+    
+    //create specular light
+    Color p1_color = Color(.6,.7,.6);
+    Vector3f p1_l = Vector3f(-100,-100,100);
+    PointLight plight = PointLight(p1_l,p1_color);
+
+
+    if (!p.intersect(ray, &thit, &in)){
         //color black
+    }
+    else {
+        //there is an intersection, loop through all the lights
+        dlight.generateLightRay(LocalGeo& in, Ray* light_ray, Color* light_color)
         
-        
-        
-    } else {
         Vector3f normal = in.getLocal().getNormal();
-        // cout << normal;
-        // Vector3f n = Vector3f(normal[0],normal[1],normal[2]);
-        
-        ambient(Color(.1,.0,.1), color);
-        Vector3f l(100,100,-100);
-        diffuse(Color(.6,.3,.6), color, Color(.6,.3,.6), normal, l);
-        Vector3f L(-100,-100,100);
-        L.normalize(); normal.normalize();
-        float Ldotn = L.dot(normal);
-        Vector3f r = -L + 2*Ldotn*normal;
+        normal.normalize();
+
         Vector3f v = ray.dir(); v.normalize();
-        specular(Color(.6,.7,.6), color, Color(.6,.7,.6), r, v, 40);
+        ambient(Color(.1,.0,.1), color);
+        diffuse(Color(.6,.3,.6), color, normal, dlight);
+        specular(Color(.6,.7,.6), color, v,normal,40,plight);
         // *color = Color(1,0,0);
     }    
     }    
     
     
     
-}
