6a0041c42bfd0cd1bfc6a475a32dc36a52846cf8
jesca <jesca116@gmail.com>
2014-03-17 20:37:09 -0700

    Update Transform.cpp

--- Transform.cpp
+++ Transform.cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
- mat3 Transform::rotate( degrees, const vec3& axis) {
-    // need to think about this
-}
 
 
-Matrix4f Transform::scale(float x, float y, float z) {
+
+Matrix4f Transform::scale(float sx, float sy, float sz) {
 	return Matrix4f(sx, 0.0, 0.0, 0.0,
 			0.0, sy, 0.0, 0.0,
 		        0.0, 0.0, sz, 0.0,
 		        0.0, 0.0, 0.0, 1.0);
 }
 
-Matrix4f Transform::translate(float x, float y, float z) {
+Matrix4f Transform::translate(float t, float ty, float tz) {
 	return Matrix4f(1.0, 0.0, 0.0, tx,
 			0.0, 1.0, 0.0, ty,
 			0.0, 0.0, 1.0, tz,
